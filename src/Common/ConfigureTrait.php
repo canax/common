e157b6473fae37d40eb58e8d0bb3e3fbe5228d43
Mikael Roos <mikael.t.h.roos@gmail.com>
2017-03-24 09:47:27 +0100

    Minor edit of docblock comment and return self.

--- src/Common/ConfigureTrait.php
+++ src/Common/ConfigureTrait.php
@@ -22,13 +22,13 @@ trait ConfigureTrait
      * Read configuration from file or array, if a file, first check in
      * ANAX_APP_PATH/config and then in ANAX_INSTALL_PATH/config.
      *
-     * @param array/string $what is an array with key/value config options
-     *                           or a file to be included which returns such
-     *                           an array.
+     * @param []|string $what is an array with key/value config options
+     *                        or a file to be included which returns such
+     *                        an array.
      *
      * @throws Exception when argument if not a filer nor an array.
      *
-     * @return $this for chaining.
+     * @return self for chaining.
      */
     public function configure($what)
     {
@@ -46,6 +46,6 @@ trait ConfigureTrait
         }
 
         $this->config = array_merge($this->config, $options);
-        return $this->config;
+        return self;
     }
 }
