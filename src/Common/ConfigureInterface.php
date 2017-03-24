25ca5570cb3ad018a6329da34513fce239b34c93
Mikael Roos <mikael.t.h.roos@gmail.com>
2017-03-24 09:27:10 +0100

    Minor edit of docblock comment.

--- src/Common/ConfigureInterface.php
+++ src/Common/ConfigureInterface.php
@@ -13,13 +13,13 @@ interface ConfigureInterface
      * Read configuration from file or array, if a file, first check in
      * ANAX_APP_PATH/config and then in ANAX_INSTALL_PATH/config.
      *
-     * @param array/string $what is an array with key/value config options
-     *                           or a file to be included which returns such
-     *                           an array.
+     * @param []|string $what is an array with key/value config options
+     *                        or a file to be included which returns such
+     *                        an array.
      *
      * @throws Exception when argument if not a filer nor an array.
      *
-     * @return $this for chaining.
+     * @return self for chaining.
      */
     public function configure($what);
 }
